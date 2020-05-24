75b5b75d170de0a25cdc729bc299ba550d6ffb6a
Michael Olson <michael.chase.olson@outlook.com>
2020-05-24 16:22:04 -0400

    [Add_Postorder_Traversal] Added Postorder traversal strategy.

---
+++ GeneralTree/TraversalStrategies/PostorderTraversalStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GeneralTree.Interfaces;
+
+namespace GeneralTree.TraversalStrategies
+{
+    public class PostorderTraversalStrategy : ITraversalStrategy
+    {
+        public void Traverse<T>(IGeneralTree<T> tree, IVisitor visitor)
+        {
+            if (!tree.RootNode.Children.Any())
+            {
+                tree.RootNode.Accept(visitor);
+                return;
+            }
+
+            var workingStack = new Stack<INode<T>>();
+            var stackForVisit = new Stack<INode<T>>();
+
+            workingStack.Push(tree.RootNode);
+
+            while (workingStack.Any())
+            {
+                var node = workingStack.Pop();
+                stackForVisit.Push(node);
+
+                foreach (var nodeChild in node.Children)
+                {
+                    workingStack.Push(nodeChild);
+                }
+            }
+
+            while (stackForVisit.Any())
+            {
+                var node = stackForVisit.Pop();
+                node.Accept(visitor);
+            }
+        }
+    }
+}
